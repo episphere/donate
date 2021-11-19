ca18892e46cd28de85e6927cbbe5e8d5193cead9
Bhaumik Patel <bhaumik55231@gmail.com>
2021-11-19 11:34:23 -0500

    Handling errors

--- FaaS/index.js
+++ FaaS/index.js
@@ -1,12 +1,10 @@
 const admin = require('firebase-admin');
 admin.initializeApp();
 const storage = admin.storage();
-const bucketName = process.env.GCSBucket || 'donation-deleteme';
-const bucket = storage.bucket(bucketName);
+const bucketName = process.env.GCSBucket;
 
-const getTokens = async (m = 10, n = 10000) => { // get tokens or create them first
+const getTokens = async (m, n = 10000, files) => { // get tokens or create them first
   try {
-    const [files] = await bucket.getFiles();
     if (files.length === 0) {
       await createTokenFiles(m, n);
     }
@@ -28,52 +26,55 @@ exports.donate = async (req, res) => {
   if (req.method === 'OPTIONS') return res.status(200).json({ code: 200 });
 
   const token = req.query.token ? req.query.token : req.headers.authorization ? req.headers.authorization.replace('Bearer ', '').trim() : null;
-  await getTokens();
-  if (!token) return res.status(401).json({ message: 'Authorization failed!', code: 401 });
-  const [files] = await bucket.getFiles();
-  if (req.method === 'POST') {
-    if (files.map(dt => dt.name).includes('userTokens.txt')) {
-      try {
-        const file = bucket.file('userTokens.txt');
-        const userTokensFile = await readData(file);
-        const userTokens = userTokensFile.split(',');
-        if (userTokens.includes(token)) {
-          const body = req.body;
+  try {
+    if(!bucketName) return res.status(400).json({ message: "Invalid bucket name, please setup an environment variable 'GCSBucket' with the appropriate bucket!", code: 400 });
+    const bucket = storage.bucket(bucketName);
+    const [files] = await bucket.getFiles();
+    await getTokens(10, 10000, files);
+    if (!token) return res.status(401).json({ message: 'Authorization failed!', code: 401 });
+    if (req.method === 'POST') {
+      if (files.map(dt => dt.name).includes('userTokens.txt')) {
+          const file = bucket.file('userTokens.txt');
+          const userTokensFile = await readData(file);
+          const userTokens = userTokensFile.split(',');
+          if (userTokens.includes(token)) {
+            const body = req.body;
 
-          if (Object.keys(body).length === 0) return res.status(400).json({ message: 'Body is empty!', code: 400 });
-          const fileName = body.fileName ? `data/${body.fileName}.json` : `data/${token}.json`
-          const data = body
-          if (data.fileName) delete data.fileName
-          await saveFile(JSON.stringify(data), bucket, fileName);
-          res.status(200).json({ message: 'Success!', code: 200 });
+            if (Object.keys(body).length === 0) return res.status(400).json({ message: 'Body is empty!', code: 400 });
+            const fileName = body.fileName ? `data/${body.fileName}.json` : `data/${token}.json`
+            const data = body
+            if (data.fileName) delete data.fileName
+            await saveFile(JSON.stringify(data), bucket, fileName);
+            res.status(200).json({ message: 'Success!', code: 200 });
+          }
+          else return res.status(401).json({ message: 'Authorization failed!', code: 401 });
+        
+      }
+    }
+
+    if (req.method === 'GET') {
+      if (files.map(dt => dt.name).includes('adminTokens.txt')) {
+        const file = bucket.file('adminTokens.txt');
+        const adminTokensFile = await readData(file);
+        const adminTokens = adminTokensFile.split(',');
+        if (adminTokens.includes(token)) {
+          const [lists] = await bucket.getFiles({ prefix: 'data/' });
+          const userFilesLists = lists.map(dt => dt.name.replace('data/', ''));
+          if (req.query.fileName) {
+            const fileName = req.query.fileName;
+            if (!userFilesLists.includes(fileName)) return res.status(404).json({ message: 'File not found!', code: 404 });
+            const file = bucket.file(`data/${fileName}`);
+            const data = await readData(file);
+            return res.status(200).json({ message: 'Success!', code: 200, data: JSON.parse(data) });
+          }
+          else return res.status(200).json({ message: 'Success!', code: 200, files: userFilesLists });
         }
         else return res.status(401).json({ message: 'Authorization failed!', code: 401 });
-      } catch (error) {
-        console.error(error.message)
-        return res.status(400).json({ message: 'Bad request!', code: 400 });
       }
     }
-  }
-
-  if (req.method === 'GET') {
-    if (files.map(dt => dt.name).includes('adminTokens.txt')) {
-      const file = bucket.file('adminTokens.txt');
-      const adminTokensFile = await readData(file);
-      const adminTokens = adminTokensFile.split(',');
-      if (adminTokens.includes(token)) {
-        const [lists] = await bucket.getFiles({ prefix: 'data/' });
-        const userFilesLists = lists.map(dt => dt.name.replace('data/', ''));
-        if (req.query.fileName) {
-          const fileName = req.query.fileName;
-          if(!userFilesLists.includes(fileName)) return res.status(404).json({ message: 'File not found!', code: 404 });
-          const file = bucket.file(`data/${fileName}`);
-          const data = await readData(file);
-          return res.status(200).json({ message: 'Success!', code: 200, data: JSON.parse(data) });
-        }
-        else return res.status(200).json({ message: 'Success!', code: 200, files: userFilesLists });
-      }
-      else return res.status(401).json({ message: 'Authorization failed!', code: 401 });
-    }
+  } catch (error) {
+    console.error(error)
+    return res.status(error.code || 400).json({ message: error.message, code: error.code  || 400});
   }
 }
 
@@ -81,9 +82,9 @@ const readData = (file) => {
   let data = '';
   return new Promise((resolve, reject) => {
     const stream = file.createReadStream();
-      stream.on('error', error => {
-        reject(error);
-      })
+    stream.on('error', error => {
+      reject(error);
+    })
       .on('data', (d) => {
         data += d;
       })
@@ -121,7 +122,7 @@ const generateRandomStrings = (size) => {
   return unique;
 }
 
-const makeTokens = (n=1,m=16,str='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') => {
-  return [...Array(n)].map(_=>[...Array(m)]
-  .map(_=>str[Math.floor(Math.random()*62)]).join(''))
+const makeTokens = (n = 1, m = 16, str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') => {
+  return [...Array(n)].map(_ => [...Array(m)]
+    .map(_ => str[Math.floor(Math.random() * 62)]).join(''))
 }
